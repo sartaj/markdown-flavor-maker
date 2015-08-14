eb2f49c235b808067216d8bb4ce82a297e528a0e
johnnyscript <mostmodernist@gmail.com>
2015-08-13 20:09:58 -0700

    more better tests!

--- test.js
+++ test.js
@@ -1,13 +1,30 @@
 var test = require('tape')
-var flavorMaker = require('./')()
+var flavorMaker = require('./')
 
-flavorMaker.bracketize('++', '++', '<span class="plusplus">', '</span>')
 
 test('test one!', function(t){
   
-  var render = flavorMaker.render('Hello, ++world++')
+  var fm = flavorMaker()
+
+  fm.bracketize('++', '++', '<span class="plusplus">', '</span>')
+  
+  var render = fm.render('Hello, ++world++')
 
   t.equals(render, '<p>Hello, <span class="plusplus">world</span></p>\n')
+
+  t.end()
+
+})
+
+test('test two!', function(t){
+  
+  var fm = flavorMaker()
+
+  fm.bracketize('$-', '-$', '<div class="moneymoney">', '</div>')
+
+  var render = fm.render('***Hello***, $-__world__-$')
+
+  t.equals(render, '<p><strong><em>Hello</em></strong>, <div class="moneymoney"><strong>world</strong></div></p>\n')
 
   t.end()
 
